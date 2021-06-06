4b5010ad4ebfbc95a16802ccf91bbb857d39c8d1
Pavel Yarmoliuk <76595343+pavel-yarmaliuk@users.noreply.github.com>
2021-06-06 14:43:36 +0300

    changed javascript functionality

--- Landviser/static/functions.js
+++ Landviser/static/functions.js
@@ -1,16 +1,55 @@
-let select = document.getElementById("VES-type-select");
-let option_1 = document.getElementById("VES-type-1");
-let option_2 = document.getElementById("VES-type-2");
 function hide_input() {
     let AB_parameter = document.getElementById("input-a-parameter");
     let k_parameter = document.getElementById("input-k-parameter");
+    let input_k = document.getElementById("input-k");
+
     if (select.value == "2"){
-        console.log("Hello world!");
         //AB_parameter.style.visibility = "hidden";
+        input_k.style.visibility = "hidden";
         k_parameter.style.visibility = "hidden";
     }else{
+        if(document.getElementById("k-mode-select").value == "Custom"){
+          input_k.style.visibility = "visible";
+        }
         AB_parameter.style.visibility = "visible";
         k_parameter.style.visibility = "visible";
     }
 }
+
+function show_input_a(){
+    let input_a = document.getElementById("input-a");
+    if(select_a_mode.value == "Custom"){
+        input_a.style.visibility = "visible";
+    }else{
+        input_a.style.visibility = "hidden";
+    }
+}
+
+function show_input_k(){
+    let input_k = document.getElementById("input-k");
+    if(select_k_mode.value == "Custom"){
+        input_k.style.visibility = "visible";
+    }else{
+        input_k.style.visibility = "hidden";
+    }
+}
+
+function show_filename(){
+    let file_name = document.getElementById("file-name");
+    file_name.textContent = file_form.value.substring(file_form.value.lastIndexOf('\\') + 1);
+}
+
+
+let select = document.getElementById("VES-type-select");
 select.addEventListener("change", hide_input, false);
+
+
+let select_a_mode = document.getElementById("a-mode-select");
+select_a_mode.addEventListener("change", show_input_a, false);
+
+let select_k_mode = document.getElementById("k-mode-select");
+select_k_mode.addEventListener("change", show_input_k, false);
+
+
+let file_form = document.getElementById("uploaded-file");
+file_form.addEventListener("change", show_filename, false);
