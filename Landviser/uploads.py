a58476cac7fbeb97bf84f187c140156c1336b337
Pavel Yarmoliuk <76595343+pavel-yarmaliuk@users.noreply.github.com>
2021-06-06 12:30:32 +0300

    added functionality of  plotting a lot of measurments

--- Landviser/uploads.py
+++ Landviser/uploads.py
@@ -3,44 +3,60 @@
 ########################################################################################
 from flask import render_template, request, Blueprint
 from werkzeug.utils import secure_filename
-import pandas as pd
 import os
-from utils.functions_1d import recalculate_table, plot_1d, calculate_pseudo_depth
+from utils.functions_1d import recalculate_table, plot_1d, calculate_pseudo_depth, filter_1d_table, \
+    ConstantsFor1dCalculation, generate_1_calculation
+import uuid
 
 uploads = Blueprint('uploads', __name__)
-uploads_dir = "static/uploads\\"  # base uploads dir
-plots_dir = "static/Images\\"
+uploads_dir = "static/uploads/"  # base uploads dir
+plots_dir = "static/Images/"
 
 
 @uploads.route('/upload', methods=['GET', 'POST'], endpoint='uploads_file')
 def uploads_file():
-    # print('UPLOADS1D')
     is_hidden_plot_block = 'hidden'
     is_hidden_plot_block_2d = 'hidden'
     if request.method == 'POST':  # if request is post then upload it to uploads path and read it as csv table
-
         f = request.files.get('file')  # getting uploaded file
         path_to_upload_file = os.path.join(uploads_dir, secure_filename(f.filename))  # creating path to upload
         f.save(path_to_upload_file)  # saving file to path with uploads
         if request.form.get('VES-type-select') == '1':
-            df = pd.read_csv(path_to_upload_file)
+            k_dict = ConstantsFor1dCalculation.default_k()
+            k_value = 1
+            try:
+                k_value = k_dict.get(request.form.get('k-mode-select')) if request.form.get(
+                    'k-mode-select') != "Custom" else float(request.form.get('k-parameter'))
+            except (ValueError, KeyError):
+                pass  # todo: find how to alert exception in UI
 
-            html_of_table_before_recalculating = df.to_html(classes=['table'])
-            html_of_table_after_recalculating = recalculate_table(df).to_html(
-                classes=['table'])  # creating html of recalculated table
-            path_to_upload_1d_plot = os.path.join(plots_dir, 'plot.png')
+            ab_modes_dict = ConstantsFor1dCalculation.ab_mode()
+            ab_dict = ConstantsFor1dCalculation.ab()
+            ab = ab_dict.get('14m')
+            ab_mode = 1
+            try:
+                ab_mode = ab_modes_dict.get(request.form.get('a-mode-select'))
+                ab = ConstantsFor1dCalculation.ab().get(request.form.get('a-mode-select'))
+            except ValueError:
+                pass  # todo: find how to alert exception in UI
 
-            calculate_pseudo_depth(df)
-            plot_1d(df, path_to_upload_1d_plot)
+            result = []
 
-            # is_hidden_plot_block = 'visible'
+            for df in generate_1_calculation(path_to_upload_file, 16):
+                filtered_df = filter_1d_table(df, ab_mode, ab, k=k_value)
+                html_of_table_before_recalculating = filtered_df.to_html(classes=['table'])
+                # creating html of recalculated table
+                html_of_table_after_recalculating = recalculate_table(filtered_df).to_html(classes=['table'])
+                calculate_pseudo_depth(filtered_df)
+
+                path_to_upload_1d_plot = os.path.join(plots_dir, f'{str(uuid.uuid4())}.png')
+                plot_1d(filtered_df, path_to_upload_1d_plot)
+                result.append({'html_of_table_before_recalculating': html_of_table_before_recalculating,
+                               'html_of_table_after_recalculating': html_of_table_after_recalculating,
+                               'path_to_upload_1d_plot': path_to_upload_1d_plot
+                               })
             return render_template('upload.html',
-                                   table_before=html_of_table_before_recalculating,
-                                   table_after=html_of_table_after_recalculating,
-                                   table_title_before="The source table is:<br>",
-                                   table_title_after="The recalculated table is:<br>",
-                                   plot_name='Plot for',
-                                   plot_path=path_to_upload_1d_plot,
+                                   result=result,
                                    is_hidden_plot_block='visible',
                                    is_hidden_plot_block_2d=is_hidden_plot_block_2d)
         elif request.form.get('VES-type-select') == '2':
