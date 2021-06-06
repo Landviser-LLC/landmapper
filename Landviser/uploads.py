64139d9d45151e86a3f31398d518769adb9c0382
Pavel Yarmoliuk <76595343+pavel-yarmaliuk@users.noreply.github.com>
2021-06-06 14:44:29 +0300

    changed upload logic
    now 3 graphs in one will be shown

--- Landviser/uploads.py
+++ Landviser/uploads.py
@@ -5,7 +5,7 @@ from flask import render_template, request, Blueprint
 from werkzeug.utils import secure_filename
 import os
 from utils.functions_1d import recalculate_table, plot_1d, calculate_pseudo_depth, filter_1d_table, \
-    ConstantsFor1dCalculation, generate_1_calculation
+    ConstantsFor1dCalculation, generate_1_calculation, concatenate_batch_of_calculations
 import uuid
 
 uploads = Blueprint('uploads', __name__)
@@ -20,7 +20,10 @@ def uploads_file():
     if request.method == 'POST':  # if request is post then upload it to uploads path and read it as csv table
         f = request.files.get('file')  # getting uploaded file
         path_to_upload_file = os.path.join(uploads_dir, secure_filename(f.filename))  # creating path to upload
-        f.save(path_to_upload_file)  # saving file to path with uploads
+        try:
+            f.save(path_to_upload_file)  # saving file to path with uploads
+        except PermissionError:
+            pass  # todo print that no file are selected
         if request.form.get('VES-type-select') == '1':
             k_dict = ConstantsFor1dCalculation.default_k()
             k_value = 1
@@ -37,24 +40,43 @@ def uploads_file():
             try:
                 ab_mode = ab_modes_dict.get(request.form.get('a-mode-select'))
                 ab = ConstantsFor1dCalculation.ab().get(request.form.get('a-mode-select'))
+                if not ab:
+                    ab = list(map(lambda x: float(x.strip()), request.form.get('a-parameter').split(',')))
             except ValueError:
                 pass  # todo: find how to alert exception in UI
 
             result = []
+            i = 1
+            list_of_df_before_recalc = []
+            list_of_df_after_recalc = []
+            # todo make elastic from calculations number
+            for df in generate_1_calculation(path_to_upload_file, len(ab)):
 
-            for df in generate_1_calculation(path_to_upload_file, 16):
-                filtered_df = filter_1d_table(df, ab_mode, ab, k=k_value)
-                html_of_table_before_recalculating = filtered_df.to_html(classes=['table'])
-                # creating html of recalculated table
-                html_of_table_after_recalculating = recalculate_table(filtered_df).to_html(classes=['table'])
-                calculate_pseudo_depth(filtered_df)
+                if i % 4 == 0:
+                    # print()
+                    raw_df = concatenate_batch_of_calculations(list_of_df_before_recalc)
 
-                path_to_upload_1d_plot = os.path.join(plots_dir, f'{str(uuid.uuid4())}.png')
-                plot_1d(filtered_df, path_to_upload_1d_plot)
-                result.append({'html_of_table_before_recalculating': html_of_table_before_recalculating,
-                               'html_of_table_after_recalculating': html_of_table_after_recalculating,
-                               'path_to_upload_1d_plot': path_to_upload_1d_plot
-                               })
+                    recalculated_dfs = concatenate_batch_of_calculations(list_of_df_after_recalc)
+                    html_of_table_before_recalculating = raw_df.to_html(classes=['table'])
+                    html_of_table_after_recalculating = recalculated_dfs.to_html(classes=['table'])
+
+                    path_to_upload_1d_plot = os.path.join(plots_dir, f'{str(uuid.uuid4())}.png')  # 'plot.png')
+                    print(recalculated_dfs)
+                    plot_1d(recalculated_dfs, path_to_upload_1d_plot)
+                    result.append({'html_of_table_before_recalculating': html_of_table_before_recalculating,
+                                   'html_of_table_after_recalculating': html_of_table_after_recalculating,
+                                   'path_to_upload_1d_plot': path_to_upload_1d_plot
+                                   })
+                    list_of_df_before_recalc.clear()
+                    list_of_df_after_recalc.clear()
+                else:
+                    filtered_df = filter_1d_table(df, ab_mode, ab, k=k_value)
+                    list_of_df_before_recalc.append(filtered_df)
+                    # creating html of recalculated table
+                    recalculated_df = recalculate_table(filtered_df.__deepcopy__())
+                    calculate_pseudo_depth(recalculated_df)
+                    list_of_df_after_recalc.append(recalculated_df)
+                i += 1
             return render_template('upload.html',
                                    result=result,
                                    is_hidden_plot_block='visible',
