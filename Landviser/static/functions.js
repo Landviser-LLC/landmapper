665948d404e87d3d61e816b9693b6be3b406650a
Pavel Yarmoliuk <76595343+pavel-yarmaliuk@users.noreply.github.com>
2021-06-07 13:27:10 +0300

    mn and ab fields are syncronized

--- Landviser/static/functions.js
+++ Landviser/static/functions.js
@@ -18,13 +18,22 @@ function hide_input() {
 
 function show_input_a(){
     let input_a = document.getElementById("input-a");
+    select_mn_mode.selectedIndex = select_a_mode.selectedIndex;
     if(select_a_mode.value == "Custom"){
         input_a.style.visibility = "visible";
     }else{
         input_a.style.visibility = "hidden";
     }
+    show_input_mn();    
 }
-
+function show_input_mn(){
+    let input_mn = document.getElementById("input-mn");
+    if(select_mn_mode.value == "Custom"){
+        input_mn.style.visibility = "visible";
+    }else{
+        input_mn.style.visibility = "hidden";
+    }
+}
 function show_input_k(){
     let input_k = document.getElementById("input-k");
     if(select_k_mode.value == "Custom"){
@@ -40,6 +49,10 @@ function show_filename(){
 }
 
 
+
+
+
+
 let select = document.getElementById("VES-type-select");
 select.addEventListener("change", hide_input, false);
 
@@ -50,6 +63,8 @@ select_a_mode.addEventListener("change", show_input_a, false);
 let select_k_mode = document.getElementById("k-mode-select");
 select_k_mode.addEventListener("change", show_input_k, false);
 
+let select_mn_mode = document.getElementById("mn-mode-select");
+select_mn_mode.addEventListener("change", show_input_mn, false);
 
 let file_form = document.getElementById("uploaded-file");
 file_form.addEventListener("change", show_filename, false);
